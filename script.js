229a7338b2003265e8422553e2727f6f8bbab149
Filip <filipskorpik3@gmail.com>
2022-03-30 22:11:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+let firstName = "Filip"
+let secondName = "Škorpík"
+let job = "Front-End Developer"
+let city = "Olomouc"
+console.log("Ahoj, jmenuji se " + firstName + " " + secondName + " a pracuji jako " + job + " ve městě " + city)   
+*/
+/*
+let student1 = 54
+let student2 = 35
+let student1Name = "Harry"
+let student2Name = "Ron"
+let maxPoints = 200
+let result1 = (student1 / maxPoints) * 100
+let result2 = (student2 / maxPoints) * 100
+
+console.log(student1Name + " dosáhl " + result1 + "% úspěšnosti.")
+console.log(student2Name + " dosáhl " + result2 + "% úspěšnosti.")
+*/
+
+let age = 80
+
+let child = age < 17
+let adult = age >= 18 || age < 65;
+let pensitioner = age >=65
+
+console.log(child)
+console.log(adult)
+console.log(pensitioner)
+
