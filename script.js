ba3ea6d6b31e8262d88a41d26bcaa52536b0c171
Filip <filipskorpik3@gmail.com>
2022-06-02 07:30:25 +0200

    ukol

--- script.js
+++ script.js
@@ -282,7 +282,7 @@
 // let number4 = Math.ceil(Math.random() * 6)
 // let number5 = Math.ceil(Math.random() * 6)
 // let number6 = Math.ceil(Math.random() * 6)
-// let sum = number1 + number2 + number3 + number4 + number5 + number5
+// let sum = number1 + number2 + number3 + number4 + number5 + number6
 // if(sum >= 25){
 //     console.log("Jsi vítěz")
 // } else {
@@ -296,7 +296,7 @@
 
 
 
-let myArray = ["jedna","dva","tři"]
+// let myArray = ["jedna","dva","tři"]
 // myArray.push("čtyři")
 // console.log(myArray)
 
@@ -310,10 +310,252 @@ let myArray = ["jedna","dva","tři"]
 // console.log(myArray)
 
 //kde začít, kolik prvků odstranit, co přidat před vybraný prvek
-myArray.splice(1,1) 
-console.log(myArray)
+// myArray.splice(1,1) 
+// console.log(myArray)
 
-myArray.splice(2,0,"Tři a půl")
-console.log(myArray)
+// myArray.splice(2,0,"Tři a půl")
+// console.log(myArray)
 
+
+// let employees = ["Hatty", "Hermiohan"]
+// employees.forEach(function(employee, index){
+//     console.log(index, employee);  
+// });
+
+// employees.forEach(function(employee, index){
+//     console.log(`${index}. ${employee}`);
+// })
+
+
+// toDo = ["sestříhat video", "uklidit pokoj", "vyluxovat"]
+
+// for(i = 0; i < toDo.length; i++){
+//     console.log(`${i + 1}. ${toDo[i]}`);
+// }
+
+// toDo.forEach(function(task, index){
+//     console.log(`${index + 1}. ${task}`);
+// })
+
+// array = []
+// for(k = 0; k < 5; k++){
+//     array.push(k)
+// }
+// console.log(array);
+
+
+
+
+// let employees = ["David", "Marek", "Jana"]
+// console.log(employees.indexOf("David"));
+// console.log(employees.indexOf("Jana"));
+// console.log(employees.indexOf("Marek"));
+
+// console.log(employees.indexOf("Filip"));
+
+// if(employees.indexOf("David") === -1){
+//     console.log("Uživatel nenalezen");
+// } else {
+//     console.log("Uživatel nalezen");
+// }
+
+// if(employees.indexOf("Filip") === -1){
+//     console.log("Uživatel nenalezen");
+// } else {
+//     console.log("Uživatel nalezen");
+// }
+
+
+// let criminals = [{
+//     firstName: "Martin",
+//     secondName: "Zelený",
+//     birth: 1985,
+//     address: "U sloupů 16",
+//     city: "České Budějovice"
+// }, {
+//     firstName: "Jana",
+//     secondName: "Růžová",
+//     birth: 1996,
+//     address: "Malská 29",
+//     city: "České Budějovice"
+// }, {
+//     firstName: "Filip",
+//     secondName: "Modrý",
+//     birth: 1989,
+//     address: "Stevardská 38",
+//     city: "České Budějovice"
+// }]
+
+// let suspect = prompt("Zadej jméno podezřelého:")
+// let result = criminals.findIndex(function(name){
+//     return name.firstName === suspect
+    
+// })
+// let resultSuspect = criminals[result]
+// console.log(`Křestní jméno: ${resultSuspect.firstName}`)
+// console.log(`Příjmení: ${resultSuspect.secondName}`)
+// console.log(`Rok narození: ${resultSuspect.birth}`)
+// console.log(`Adresa: ${resultSuspect.address}`)
+// console.log(`Město: ${resultSuspect.city}`)
+
+
+
+
+
+// let criminals = [{
+//     firstName: "Martin",
+//     secondName: "Zelený",
+//     birth: 1985,
+//     licencePlate: "85C3322",
+//     address: "U sloupů 16",
+//     city: "České Budějovice"
+// }, {
+//     firstName: "Jana",
+//     secondName: "Růžová",
+//     birth: 1996,
+//     licencePlate: "93K3922",
+//     address: "Malská 29",
+//     city: "České Budějovice"
+// }, {
+//     firstName: "Filip",
+//     secondName: "Modrý",
+//     birth: 1989,
+//     licencePlate: "2EP6328",
+//     address: "Stevardská 38",
+//     city: "České Budějovice"
+// }]
+
+// let witness = prompt()
+// let arrayResult = criminals.filter(function(oneSuspect){
+//     let tryFind = oneSuspect.licencePlate.toLowerCase().includes(witness)
+//     return tryFind
+// })
+
+// arrayResult.forEach(function(oneInfo){
+//     console.log(`Jméno: ${oneInfo.firstName}`);
+//     console.log(`Příjmení: ${oneInfo.secondName}`);
+//     console.log(`Rok narození: ${oneInfo.birth}`);
+//     console.log(`Poznávací značka: ${oneInfo.licencePlate}`);
+//     console.log(`Adresa: ${oneInfo.address}`);
+//     console.log(`Město: ${oneInfo.city}`);
+//     console.log(`---------------`); 
+// })
+
+
+
+
+
+
+
+// ***********************************************************************
+// + html
+
+// let heading1 = document.querySelector("h1")
+// console.log(heading1);
+
+// let heading11 = document.querySelector("#test2")
+// console.log(heading11);
+
+// let heading2 = document.querySelectorAll("h2")
+// console.log(heading2[0]);
+// console.log(heading2[1]);
+
+// let pAllTest1 = document.querySelectorAll(".test1")
+// console.log(pAllTest1[0]);
+// console.log(pAllTest1[1]);
+
+// let pTest1 = document.getElementsByClassName("test1")
+// console.log(pTest1[0]);
+// console.log(pTest1[1]);
+
+// let pTestId = document.getElementById("test2")
+// console.log(pTestId);
+
+
+
+// let paragraphs = document.querySelectorAll("p")
+// paragraphs.forEach(function(oneParagraph){
+//     console.log(oneParagraph.textContent);
+// })
+
+
+
+// let paragraphs = document.querySelectorAll("p")
+
+// paragraphs.forEach(function(oneParagraph){
+//     if(oneParagraph.textContent.toLowerCase().includes("nakrmit")){
+//         oneParagraph.remove()
+//     }
+// })
+
+
+
+// let newParagraph = document.createElement("p")
+// newParagraph.textContent = "nový úkol"
+// document.querySelector("header").appendChild(newParagraph)
+
+// const newDiv = document.createElement("div")
+// document.querySelector("header").appendChild(newDiv)
+
+// let secondParagraph = document.createElement("p")
+// secondParagraph.textContent = "testovací odstavec do divu"
+// newDiv.appendChild(secondParagraph)
+
+// secondParagraph.append("text na začátek")
+// secondParagraph.prepend("text na konec")
+
+// ZMĚNA OBSAHU PO KLIKNUTÍ
+// document.querySelector("h1").addEventListener("click", function(event){
+//     event.target.textContent = "klikni na nadpis níže"
+// })
+
+// document.querySelector("h2").addEventListener("click", function(event){
+//     event.target.textContent = "klikni na nadpis níže"
+// })
+
+// document.querySelector("h3").addEventListener("click", function(event){
+//     event.target.textContent = "hotovo"
+// })
+
+
+let criminals = [{
+    firstName: "Martin",
+    secondName: "Zelený",
+    birth: 1985,
+    licencePlate: "85cc322",
+    address: "U sloupů 16",
+    city: "České Budějovice"
+}, {
+    firstName: "Jana",
+    secondName: "Růžová",
+    birth: 1996,
+    licencePlate: "85b32",
+    address: "Malská 29",
+    city: "České Budějovice"
+}, {
+    firstName: "Filip",
+    secondName: "Modrý",
+    birth: 1989,
+    licencePlate: "asd3",
+    address: "Stevardská 38",
+    city: "České Budějovice"
+}]
+
+
+// uložení dat z políčka
+let filters = {
+    searchText: ""
+}
+
+//Filtr
+const renderCriminals = function(ourCriminal, tryToFind){
+
+}
+
+// načtení dat z políčka
+let licencePlate = document.querySelector("#licence-plate")
+licencePlate.addEventListener("input", function(event){
+    filters.searchText = event.target.value
+    renderCriminals(criminals, filters)
+})
 
---
+++ toDoApp.js
@@ -0,0 +1,46 @@
+myToDos = [{
+    text: "Vynést koš",
+    completion: false
+},{
+    text: "Nakoupit",
+    completion: false
+},{
+    text: "Uklidit",
+    completion: true
+},{
+    text: "nakrmit psa",
+    completion: true
+},{
+    text: "nakrmit kočku",
+    completion: false
+}]
+// filtrování počtu nesplněných úkolů
+let toDoLeft = myToDos.filter(function(oneToDo){
+    return oneToDo.completion === false
+})
+
+// vypsání počtu zbývajících úkolů
+let newParagraph = document.createElement("p")
+newParagraph.textContent = `Zbývá úkolů: ${toDoLeft.length}`
+document.querySelector(".tasks").appendChild(newParagraph)
+
+
+// for(let i = 0; i < myToDos.length; i++){
+//     let paragraph = document.createElement("p")
+//     paragraph.textContent = myToDos[i].text
+//     document.querySelector(".tasks").appendChild(paragraph)
+// }
+
+// vypsání konkrétních zbývajících úkolů
+let par = myToDos.forEach(function(oneDo){
+    if(oneDo.completion === false){
+        let paragraph = document.createElement("p")
+        paragraph.textContent = oneDo.text
+        document.querySelector(".tasks").appendChild(paragraph)
+    }
+})
+
+
+document.querySelector(".btn").addEventListener("click", function(event){
+    console.log("kliknuto");
+})
